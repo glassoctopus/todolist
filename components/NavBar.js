6a67772727d77c113eb4944f39aa35187393c8c1
Thomas McMahon <glassoctopus@yahoo.com>
2024-07-17 19:23:05 -0500

    set up database and starting on front end for a d6 character sheet.

--- components/NavBar.js
+++ components/NavBar.js
@@ -13,7 +13,7 @@ export default function NavBar() {
   return (
     <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
       <Container>
-        <Link passHref href="/">
+        <Link passHref href="/todos">
           <Navbar.Brand>My List of To-dos </Navbar.Brand>
         </Link>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
@@ -25,6 +25,15 @@ export default function NavBar() {
             </Link>
             <Link passHref href="/todos">
               <Nav.Link>What is next</Nav.Link>
+            </Link>
+            <Link passHref href="/heros">
+              <Nav.Link>Make a Hero?</Nav.Link>
+            </Link>
+            <Link passHref href="/heros/archtypes/new">
+              <Nav.Link>Make a Character Type</Nav.Link>
+            </Link>
+            <Link passHref href="/equipment">
+              <Nav.Link>equipment</Nav.Link>
             </Link>
             <Button variant="danger" onClick={signOut}>
               Sign Out
---
+++ pages/heros/archtypes/update/[id].js
@@ -0,0 +1,24 @@
+import { useRouter } from 'next/router';
+import { useEffect, useState } from 'react';
+import ArchtypeForm from '../../../../components/character/forms/ArchtypeForm';
+import { useAuth } from '../../../../utils/context/authContext';
+import { getSingleArchtype } from '../../../../utils/data/archtypeData';
+
+export default function NewArchType() {
+  const router = useRouter();
+  const { id } = router.query;
+  const { user } = useAuth();
+  const { anArchtype, setAnArchtype } = useState({});
+
+  useEffect(() => {
+    getSingleArchtype(id).then(setAnArchtype);
+  }, [id, setAnArchtype]);
+  return (
+    <div className="hero-container">
+      <div className="hero-content">
+        <h2>Create a new character Archtype</h2>
+        <ArchtypeForm user={user} id={Number(id)} update={anArchtype} />
+      </div>
+    </div>
+  );
+}
