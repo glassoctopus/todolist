026f6ce28cc03f0b9cddffc57a3128a3a9f88557
Thomas McMahon <glassoctopus@yahoo.com>
2024-07-17 19:28:23 -0500

    weird

--- pages/heros/archtypes/index.js
+++ pages/heros/archtypes/index.js
@@ -1,8 +1,8 @@
-import React, { useEffect, useState } from 'react';
+// import React, { useEffect, useState } from 'react';
 import Link from 'next/link';
 import { Button } from 'react-bootstrap';
-import { useAuth } from '../../../utils/context/authContext';
-import { getArchtypes } from '../../../utils/data/archtypeData';
+// import { useAuth } from '../../../utils/context/authContext';
+// import { getArchtypes } from '../../../utils/data/archtypeData';
 
 function Hero() {
 //   const [archtype, setArchtype] = useState([]);
--- pages/heros/index.js
+++ pages/heros/index.js
@@ -1,8 +1,8 @@
-import React, { useEffect, useState } from 'react';
+// import React, { useEffect, useState } from 'react';
 import Link from 'next/link';
 import { Button } from 'react-bootstrap';
-import { useAuth } from '../../utils/context/authContext';
-import { getHeros } from '../../utils/data/heroData';
+// import { useAuth } from '../../utils/context/authContext';
+// import { getHeros } from '../../utils/data/heroData';
 
 function Hero() {
 //   const [heros, setHeros] = useState([]);
